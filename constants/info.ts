f7e7606929193857f5b636d35a825da77be216a7
Sara Vieira <hey@iamsaravieira.com>
2022-08-22 17:43:39 +0000

    Update 5 files

---
+++ constants/info.ts
@@ -0,0 +1,4 @@
+export const PLATFORMS = {
+    NES: 18
+}
+export const FIELDS = ["name", "screenshots.*", "cover.*", "videos.*", "first_release_date", "franchises.*", "rating", "rating_count", "slug", "storyline", "summary", "url", "involved_companies.*"]

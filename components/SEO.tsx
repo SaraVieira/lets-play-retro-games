3d94fb26354ca5cbdd33717e3bd3cebae4e591b1
Sara Vieira <hey@iamsaravieira.com>
2022-09-05 17:03:13 +0300

    add meta

---
+++ components/SEO.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+
+export const SEO = () => (
+  <Head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <title>Let{"'"}s Play Retro Games</title>
+    <meta name="title" content="Let's Play Retro Games" />
+    <meta
+      name="description"
+      content="Find new old games to play and love every second of it"
+    />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://metatags.io/" />
+    <meta property="og:title" content="Let's Play Retro Games" />
+    <meta
+      property="og:description"
+      content="Find new old games to play and love every second of it"
+    />
+    <meta property="og:image" content="https://letsplayretro.games/meta.png" />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://metatags.io/" />
+    <meta property="twitter:title" content="Let's Play Retro Games" />
+    <meta
+      property="twitter:description"
+      content="Find new old games to play and love every second of it"
+    />
+    <meta
+      property="twitter:image"
+      content="https://letsplayretro.games/meta.png"
+    />
+  </Head>
+)
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import 'tuicss'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { Header } from '../components/Header'
-import Head from 'next/head'
+import { SEO } from '../components/SEO'
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -13,26 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
+      <SEO />
       <div className="min-h-screen tui-bg-cyan-white">
         <Header />
         <div className="flex min-h-screen items-center justify-center">

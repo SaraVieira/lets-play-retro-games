5d7e0d7308b9c5cebb3760ef094b5bdda5a843ba
Sara Vieira <hey@iamsaravieira.com>
2022-09-06 12:38:53 +0300

    add search api endpoint

---
+++ pages/api/search.ts
@@ -0,0 +1,26 @@
+import { prisma } from '../../prisma/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
+    const { query } = req.query as unknown as { query: string }
+
+    const games = await prisma.game.findMany({
+        select: {
+            id: true,
+            igdb_id: true,
+            first_release_date: true,
+            total_rating: true,
+            name: true,
+            slug: true,
+            console: true
+        },
+        where: {
+            name: {
+                search: query,
+            },
+        },
+    })
+    res.status(200).json(games)
+}
+
+export default handler

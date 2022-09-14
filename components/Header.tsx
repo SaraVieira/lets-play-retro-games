6e4486dd64f789d08d0850a587a957169e7698ca
Sara Vieira <hey@iamsaravieira.com>
2022-09-14 11:23:32 +0300

    menu fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,115 +6,124 @@ import { Logo } from './Logo'
 export const Header = () => {
   const { asPath } = useRouter()
   return (
-    <nav className="tui-nav !flex sm:justify-between">
-      <Link href={'/'}>
-        <a>
-          <Logo />
-        </a>
-      </Link>
-      <div className="!flex gap-6 ml-20">
+    <nav className="tui-nav !flex justify-between ">
+      <div>
         <Link href={'/'}>
-          <a className="hidden sm:block">
-            <span className="red-168-text">H</span>ome
-          </a>
-        </Link>
-
-        <li className="tui-dropdown">
-          <span className="red-168-text">R</span>andom{' '}
-          <span className="hidden sm:block">
-            <span className="red-168-text ">G</span>ame
-          </span>
-          <div className="tui-dropdown-content">
-            <ul>
-              {menuMain.map((console) => (
-                <li className="tui-dropdown block" key={console.name}>
-                  <span className="right">►</span>
-                  <span className="red-168-text">{console.name.charAt(0)}</span>
-                  {console.name.substring(1)}
-                  <div className="tui-dropdown-content">
-                    <ul>
-                      {console.items.map((item) => (
-                        <li key={item.id}>
-                          <Link href={item.linkRandom}>
-                            <a>
-                              <span className="red-168-text">
-                                {item.name.charAt(0)}
-                              </span>
-                              {item.name.substring(1)}
-                            </a>
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </li>
-              ))}
-              {otherInMenu.map((console) => (
-                <li key={console.name}>
-                  <a href={console.linkRandom}>
-                    <span className="red-168-text">
-                      {console.name.charAt(0)}
-                    </span>
-                    {console.name.substring(1)}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </li>
-        <li className="tui-dropdown">
-          <span className="red-168-text">A</span>ll{' '}
-          <span className="red-168-text">G</span>ames
-          <div className="tui-dropdown-content">
-            <ul>
-              {menuMain.map((console) => (
-                <li className="tui-dropdown block" key={console.name}>
-                  <span className="right">►</span>
-                  <span className="red-168-text">{console.name.charAt(0)}</span>
-                  {console.name.substring(1)}
-                  <div className="tui-dropdown-content">
-                    <ul>
-                      {console.items.map((item) => (
-                        <li key={item.id}>
-                          <Link href={item.linkAll}>
-                            <a>
-                              <span className="red-168-text">
-                                {item.name.charAt(0)}
-                              </span>
-                              {item.name.substring(1)}
-                            </a>
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </li>
-              ))}
-              {otherInMenu.map((console) => (
-                <li key={console.name}>
-                  <a href={console.linkAll}>
-                    <span className="red-168-text">
-                      {console.name.charAt(0)}
-                    </span>
-                    {console.name.substring(1)}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </li>
-
-        <Link href={'/search'}>
           <a>
-            <span className="red-168-text">S</span>earch
+            <Logo />
           </a>
         </Link>
+        <div className="flex gap-4 sm:gap-6 sm:ml-[120px] ml-20 w-full">
+          <Link href={'/'}>
+            <a className="hidden sm:inline">
+              <span className="red-168-text">H</span>ome
+            </a>
+          </Link>
 
-        {asPath.includes('random') && (
-          <button className="hidden sm:block" onClick={() => location.reload()}>
-            <span className="red-168-text">A</span>nother
-          </button>
-        )}
+          <li className="tui-dropdown">
+            <span className="red-168-text">R</span>andom{' '}
+            <span className="hidden sm:inline">
+              <span className="red-168-text ">G</span>ame
+            </span>
+            <div className="tui-dropdown-content">
+              <ul>
+                {menuMain.map((console) => (
+                  <li className="tui-dropdown block" key={console.name}>
+                    <span className="right">►</span>
+                    <span className="red-168-text">
+                      {console.name.charAt(0)}
+                    </span>
+                    {console.name.substring(1)}
+                    <div className="tui-dropdown-content">
+                      <ul>
+                        {console.items.map((item) => (
+                          <li key={item.id}>
+                            <Link href={item.linkRandom}>
+                              <a>
+                                <span className="red-168-text">
+                                  {item.name.charAt(0)}
+                                </span>
+                                {item.name.substring(1)}
+                              </a>
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </li>
+                ))}
+                {otherInMenu.map((console) => (
+                  <li key={console.name}>
+                    <a href={console.linkRandom}>
+                      <span className="red-168-text">
+                        {console.name.charAt(0)}
+                      </span>
+                      {console.name.substring(1)}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+          <li className="tui-dropdown">
+            <span className="red-168-text">A</span>ll{' '}
+            <span className="red-168-text">G</span>ames
+            <div className="tui-dropdown-content">
+              <ul>
+                {menuMain.map((console) => (
+                  <li className="tui-dropdown block" key={console.name}>
+                    <span className="right">►</span>
+                    <span className="red-168-text">
+                      {console.name.charAt(0)}
+                    </span>
+                    {console.name.substring(1)}
+                    <div className="tui-dropdown-content">
+                      <ul>
+                        {console.items.map((item) => (
+                          <li key={item.id}>
+                            <Link href={item.linkAll}>
+                              <a>
+                                <span className="red-168-text">
+                                  {item.name.charAt(0)}
+                                </span>
+                                {item.name.substring(1)}
+                              </a>
+                            </Link>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </li>
+                ))}
+                {otherInMenu.map((console) => (
+                  <li key={console.name}>
+                    <a href={console.linkAll}>
+                      <span className="red-168-text">
+                        {console.name.charAt(0)}
+                      </span>
+                      {console.name.substring(1)}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+
+          <Link href={'/search'}>
+            <a>
+              <span className="red-168-text">S</span>earch
+            </a>
+          </Link>
+
+          {asPath.includes('random') && (
+            <button
+              className="hidden sm:block"
+              onClick={() => location.reload()}
+            >
+              <span className="red-168-text">A</span>nother
+            </button>
+          )}
+        </div>
       </div>
       <span
         className="tui-datetime hidden sm:block"

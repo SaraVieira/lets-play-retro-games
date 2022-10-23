c0a307f3be03c386c1f6f5650e57a2191215d53a
Sara Vieira <hey@iamsaravieira.com>
2022-10-23 20:08:10 +0200

    fix CI

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,12 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { SessionWithID } from '../../../constants/types'
 import { prisma } from '../../../prisma/prisma'
 
 export const authOptions = {
   callbacks: {
-    async session({ session }) {
+    async session({ session }: { session: SessionWithID }) {
       if (!session?.user?.email) return { expires: new Date().toDateString() }
       const idUser = await prisma.user.findUnique({
         where: {
@@ -23,8 +24,8 @@ export const authOptions = {
   },
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
 }
